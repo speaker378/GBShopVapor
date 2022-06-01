bae75ed57395f37954428a9ce2f55e10ea95b6c1
speaker378 <speaker378debua@gmail.com>
2022-06-01 22:52:17 +0500

    login and logout

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,6 +6,8 @@ func routes(_ app: Application) throws {
     
     app.post("register", use: controller.register)
     app.post("change_user_data", use: controller.changeUserData)
+    app.post("login", use: controller.login)
+    app.post("logout", use: controller.logout)
     
     app.get("hello") { req -> String in
         return "Hello, world!"
