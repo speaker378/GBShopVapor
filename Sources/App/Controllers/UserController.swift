66f506980b702b98aaf5befd84b088d741642314
speaker378 <speaker378debua@gmail.com>
2022-06-01 20:42:55 +0500

    change user data

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -39,4 +39,51 @@ class UserController {
         }
         return result
     }
+    
+    func changeUserData(_ req: Request) throws -> EventLoopFuture<DefaultResponse> {
+        guard let body = try? req.content.decode(User.self) else {
+            throw Abort(.badRequest)
+        }
+        
+        print(body)
+        
+        let users = User.query(on: req.db).all()
+        let result: EventLoopFuture<DefaultResponse> = users.map { (users: [User]) -> DefaultResponse in
+            guard let user = users.first(where: { $0.id == body.id }) else {
+                return DefaultResponse(
+                    result: 0,
+                    userMessage: nil,
+                    errorMessage: "No such user"
+                )
+            }
+            
+            let filtered = users.filter {($0.login == body.login) || ($0.email == body.email)}
+            guard filtered.isEmpty else {
+                return DefaultResponse(
+                    result: 0,
+                    userMessage: nil,
+                    errorMessage: "login or email already exists"
+                )
+            }
+            
+            var response: DefaultResponse
+            response = DefaultResponse(
+                result: 1,
+                userMessage: "Successfully changed user data",
+                errorMessage: nil
+            )
+            user.login = body.login
+            user.passwordHash = body.passwordHash
+            user.email = body.email
+            user.gender = body.gender
+            user.creditCard = body.creditCard
+            user.bio = body.bio
+            user.name = body.name
+            user.lastName = body.lastName
+            let _ = user.update(on: req.db)
+            
+            return response
+        }
+        return result
+    }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,6 +5,7 @@ func routes(_ app: Application) throws {
     let controller = UserController()
     
     app.post("register", use: controller.register)
+    app.post("change_user_data", use: controller.changeUserData)
     
     app.get("hello") { req -> String in
         return "Hello, world!"
