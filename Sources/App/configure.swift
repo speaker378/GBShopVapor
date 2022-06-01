8c4f90a6281c2ced07da114a527756d1ae48ff6b
speaker378 <speaker378debua@gmail.com>
2022-06-01 14:15:23 +0500

    config database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,16 +6,16 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
-
-
+    let databaseURL = Environment.get("DATABASE_URL") ?? "postgres://kzoqfxttifhcuf:872bd6663a0f1487ca96726d71004c8aa8c74d69cd6bb7e2627d39a75401e287@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/dbtohqil5l40qa"
+    
+    if var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        postgresConfig.tlsConfiguration = .makeClientConfiguration()
+        postgresConfig.tlsConfiguration?.certificateVerification = .none
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        // ...
+    }
+    
     // register routes
     try routes(app)
 }
